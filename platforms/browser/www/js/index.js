20216f9dd7be4922bfb2ccffe3bd2d7d488f44b9
Gagan Deep <the.one.above.all.titan@gmail.com>
2018-04-07 15:33:37 +0530

    Distribution of work

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,6 +1,25 @@
-document.addEventListener('deviceready', init(), false);
+document.addEventListener('deviceready',init(),false);
 
-function init(){
-  alert("js running");
-}
+init ( function(){
+        alert('ready');
+        loadProfile();
+        alert($('#pName').val(localStorage.pName))
+        $('#pSum').click(function(){
+            profile.saveProfile();
+        });
+       /* alert('click is fired');
+        $('a[href="#monday"]').attr('href', 'http://www.live.com/')
+        //$('#mark').attr("href","#profile");*/
+ 
+});
+
+    loadProfile(function(){
+        alert('inside loadprofile');
+        alert($('#pName').val(localStorage.pName));
+    });
+    saveProfile(function(){
+        localStorage.pName=$('#pName').val();
+        localStorage.pPollege=$('#pCollege').val();
+        localStorage.pAge=$('#pAge').val();
+    });
 
