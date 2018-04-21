04da392183723847d94929e80c73d072a743fed3
TheOneAboveAllTitan <the.one.above.all.titan@gmail.com>
2018-04-21 18:51:43 +0530

    necessary js

--- platforms/browser/www/js/index.js
+++ platforms/browser/www/js/index.js
@@ -1,36 +1,69 @@
 /*The init function runs when the html page is completely loaded tweaking here should be done carefully and should be reprted.
 It is a request to everyone to start there work at the end of this document againt there corressponding comments
 Also suggest if you can make the code more efficient*/
-$('#S2Name').val('mohogany');
-alert($('#S2Name').val());
-$('#EditSubject').trigger('create');
+function subject(name, no_class,absent,present) {
+    this.name = name;
+    this.no_class=no_class;
+    this.present=present;
+    this.absent=absent;
+    }
 
-alert("this works?");
+subject.Present=function(){
+    this.present++;
+    this.no_class++;
+}
+
+subject.Absent=function(){
+    this.absent++;
+    this.no_class++;
+}
+alert("will it reach here");
+var S = new Array(10);
+for(var i=0;i<10;++i){
+    S[i]=new subject();
+}
+
+S[0].name="Physics";
+S[0].no_class=4;
+S[0].absent=1;
+S[0].present=3;
+
+var test = new subject('gagan',3,1,2);
+
+for(var i=0;S[i].name!=undefined;++i)
+{
+    x="S"+i+"name";
+    localStorage.x=S[0].name;
+    alert(localStorage.S0name);
+}
+
+localStorage.Sname=test.name;
+S[1]=localStorage.S0;
+alert(test.name);
+alert(localStorage.S0name);
+
+
+
 
 document.addEventListener('deviceready',init(),false);
 
 function init()
 {
-    $('#S2Name').val('mohogany');
-alert($('#S2Name').val());
-$('#EditSubject').trigger('create');
+    
+//EDIT TEAM
 
 
-    alert('vgffg');
-    $('#S2Name').val($('#S1Name').val());
-    alert("performed");
-    loadProfile();
     $(document).on('click',"#SubSubmit",function(){
-        alert("inside submit");
-        alert("S1Name.val() : "+ $('#S1Name').val());
         localStorage.S1Name=$("#S1Name").val();
         //check if it is stored
         alert(localStorage.S1Name);
         $('#S1Name').attr("placeholder",localStorage.S1Name);
     });
-    $(document).on('click','#place',function(){
+    
+    
+    $(document).on('click','#EditSubjectBtn',function(){
         alert("inside place holder");
-        $('#S2Name').val($('#S1Name').val());
+        $('#S2Name').val("mahogany");
         alert($("#S2Name").val());
     });
     
@@ -72,7 +105,12 @@ subject.Absent=function(){
     this.absent++;
     this.no_class++;
 }
-
+alert("will it reach here");
+var S = new Array(10);
+for(var i=0;i<10;++i){
+    S[i]=new subject();
+    alert(S[i].name);
+}
 
     
     function ahoy(){
